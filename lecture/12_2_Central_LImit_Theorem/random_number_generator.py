b5887b479e3f680fe6764836bac48d2c8456f594
hws2002 <hanys21@mails.tsinghua.edu.cn>
2023-05-30 09:24:26 +0800

    add : explanation for normal_random_number_generate function

--- lecture/12_2_Central_LImit_Theorem/random_number_generator.py
+++ lecture/12_2_Central_LImit_Theorem/random_number_generator.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# @PARAM: mean, std (default = 0,1)
+# @RETURN: a random number from normal distribution
 def normal_random_number_generator(mean = 0,std = 1):
     numbers = np.random.uniform(0,1,size=12)
     y = np.sum(numbers) - 6
